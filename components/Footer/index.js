e8caeb1b0f6a49678a345576e3cb5ebd59b22449
Elon <625598479@qq.com>
2019-11-13 16:05:12 +0800

    change footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,7 +8,8 @@ const Header = () => (
     <div className="footer-content">
       <div className="footer-item dot">HJF</div>
       <div className="footer-item dot">学如逆水行舟，不进则退</div>
-      <a target="_blank" href="https://github.com/HJianfeng/learn" className="footer-item hover">Github</a>
+      <a target="_blank" href="https://github.com/HJianfeng/learn" className="footer-item hover dot">Github</a>
+      <a target="_blank" href="http://www.beian.miit.gov.cn/" className="footer-item hover">闽ICP备19023032号</a>
     </div>
   </div>
 )
