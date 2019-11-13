42b457ddadab0370149d87f30b020ee8b04a9826
Elon <625598479@qq.com>
2019-11-13 16:12:34 +0800

    test

--- pages/editor/index.js
+++ pages/editor/index.js
@@ -9,7 +9,7 @@ import { wordCount } from '@/utils';
 import './index.less';
 
 const Editor = dynamic(import('react-markdown-editor-lite'), {
-  ssr: false
+  ssr: true
 });
 
 function ArticleEditor({ articleData }) {
