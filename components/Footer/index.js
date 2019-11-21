60ab7ea70cf65b28ee1cd7e2aa2140b8d6379f54
Elon <625598479@qq.com>
2019-11-21 11:47:40 +0800

    change footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,8 +8,19 @@ const Header = () => (
     <div className="footer-content">
       <div className="footer-item dot">HJF</div>
       <div className="footer-item dot">学如逆水行舟，不进则退</div>
-      <a target="_blank" href="https://github.com/HJianfeng/learn" className="footer-item hover dot">Github</a>
-      <a target="_blank" href="http://www.beian.miit.gov.cn/" className="footer-item hover">闽ICP备19023032号</a>
+      <a target="_blank" href="https://github.com/HJianfeng/learn" className="footer-item hover">Github</a>
+      
+    </div>
+    <div className="footer-content" style={{marginTop: '10px'}}>
+      <a target="_blank" href="http://www.beian.miit.gov.cn/" className="footer-item hover dot">闽ICP备19023032号</a>
+      <a
+        className="footer-item hover"
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=35021302000293"
+      >
+          <img src="../../static/gov.png" />
+          <p>闽公网安备 35021302000293号</p>
+      </a>
     </div>
   </div>
 )
