4204706f7ad6fcbb2dba1ced6eb751da59a140e7
Elon <625598479@qq.com>
2019-11-25 17:57:15 +0800

    up

--- components/EditorComponents/editor.js
+++ components/EditorComponents/editor.js
@@ -4,12 +4,15 @@ import Editor from 'react-markdown-editor-lite';
 
 function EditorTool({ renderHTML, value, onChange }) {
   return (
-    <Editor
-      className="editor-content"
-      renderHTML={renderHTML}
-      value={value}
-      onChange={onChange}
-    />
+    <div>
+      2
+      <Editor
+        className="editor-content"
+        renderHTML={renderHTML}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 }
 
